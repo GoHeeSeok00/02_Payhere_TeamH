1810ca32bbe243a7f37af6dcdf174e03b62f25f1
Github <weeds1590@gmail.com>
2022-07-04 18:14:56 +0900

    Docs : PAYHERE-13 BaseTimeStamp 모델 주석 수정

--- config/models.py
+++ config/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class BaseTimeStamp(models.Model):
+    """
+    Assignee : 희석
+
+    created_at, updated_at(DateTimeField) 필드를
+    사용하는 모델을 위한 기본 모델입니다.
+    abstract = True 설정을 해서 물리적 테이블이 생성되지 않습니다.
+    """
+
     created_at = models.DateTimeField("작성시간", auto_now_add=True)
     updated_at = models.DateTimeField("수정시간", auto_now=True)
 
