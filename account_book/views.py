2cf5726ac38238714b0dfaf071e813f86f0abf5a
sangbaek <aumsbk@naver.com>
2022-07-05 19:38:40 +0900

    Feat : PAYHERE-17 가계부 생성 기능 - POST api/v1/accountbooks/ 로 요청 시, 가계부 데이터 생성 기능 추가 - AccountBooksModelSerializer에서 create 메소드 override로 로그인된 user로 AccountBook 모델 생성

--- account_book/views.py
+++ account_book/views.py
@@ -1,26 +1,26 @@
-from rest_framework import permissions, status
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from config.permissions import IsOwner
 
 from .models import AccountBook
-from .serializers import AccountBookRecordCreateSerializer, AccountBooksModelSerializer
+from .serializers import AccountBooksModelSerializer
 
 
-# url : GET api/v1/accountbooks/, POST api/v1/accountbooks/
+# url : GET, POST api/v1/accountbooks/
 class AccountBooksAPIView(APIView):
     """
     Assignee : 상백
     Http method = GET, POST
 
-    클라이언트의 요청으로 이제까지 기록한 가계부 리스트 정보를
-    response 하는 API
+    GET : 클라이언트의 요청으로 이제까지 기록한 가계부 리스트 정보를 response 하는 API
 
-    POST 요청 시, 가계부 데이터를 생성
+    POST : 클라이언트의 요청 및 JSON 형태 데이터 입력 시, 가계부 데이터를 생성
+    ex) {"title": "서가앤쿡 목동점","balance": "100000"}
     """
 
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsOwner]
 
     def get(self, request):
         # 쿼리 파라미터가 들어왔을 때, 삭제된 내역만 볼 수 있게끔 구현해야 함
@@ -29,7 +29,8 @@ class AccountBooksAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = AccountBookRecordCreateSerializer(data=request.data)
+        context = {"user": request.user}
+        serializer = AccountBooksModelSerializer(data=request.data, context=context)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
