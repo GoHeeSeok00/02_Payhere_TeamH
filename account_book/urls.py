d8e81e8a1ed3b94506df0b6785471c4415ba4e22
sangbaek <aumsbk@naver.com>
2022-07-07 11:38:39 +0900

    Feat : PAYHERE-21 가계부 단건 상세 조회 기능 추가 - 가계부 단건 기록 조회 기능 추가 - 설정한 permission_classes로 인해 특정 기록의 작성자가 아닐 경우, 접근 제한

--- account_book/urls.py
+++ account_book/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("api/v1/accountbooks/", AccountBooksAPIView.as_view()),
     path("api/v1/accountbooks/<obj_id>/", AccountBooksDetailAPIView.as_view()),
     path("api/v1/accountbooks/<obj_id>/records/", AccountBooksRecordAPIView.as_view()),
+    path("api/v1/accountbooks/records/<obj_id>/", AccountBooksRecordAPIView.as_view()),
 ]
